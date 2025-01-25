c2dc406977c3bf05f4fc14cf1b2452f32d66635c
kubula316 <kubula316@o2.pl>
2025-01-26 00:07:19 +0100

    init

---
+++ Backend/UserService/src/test/java/com/rebelcommits/tubewatch/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rebelcommits.tubewatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
